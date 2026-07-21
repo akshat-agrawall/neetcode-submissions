ac235c916c196bf5af45bc1b6aea4d55e6f42aa9
Akshat Agrawal <23052863@kiit.ac.in>
2026-07-21 19:43:04 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void backtrack(int index,vector<int>& nums,vector<vector<int>> &ans,vector<int>& curr){
+        ans.push_back(curr);
+        for(int i=index;i<nums.size();i++){
+            if(i>index && nums[i] == nums[i-1])
+            continue;
+            curr.push_back(nums[i]);
+            backtrack(i+1,nums,ans,curr);
+            curr.pop_back();
+
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        vector<int> curr;
+        backtrack(0,nums,ans,curr);
+        return ans;
+  }
+};
+
