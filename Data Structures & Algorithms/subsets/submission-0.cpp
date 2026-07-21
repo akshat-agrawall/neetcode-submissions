25fa463d7088cb9193854d3ee5e08a34c97fc1fb
Akshat Agrawal <23052863@kiit.ac.in>
2026-07-21 17:44:17 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void backtrack(int index,vector<int>& nums,vector<vector<int>> &ans,vector<int>& curr){
+        ans.push_back(curr);
+        for(int i=index;i<nums.size();i++){
+            curr.push_back(nums[i]);
+            backtrack(i+1,nums,ans,curr);
+            curr.pop_back();
+
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        backtrack(0,nums,ans,curr);
+        return ans;
+  }
+};
